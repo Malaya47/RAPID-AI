83ee36d87cc93d190b54b795232fef8ae1d08583
Malaya Tiwari <sidzilegend@gmail.com>
2025-08-04 14:21:36 +0530

    update: crds data and description

--- app/data.ts
+++ app/data.ts
@@ -1,34 +1,40 @@
 import { FileEdit, Globe, Layout, Sparkles, Video, Zap } from "lucide-react";
 
 export const FeatureData = [
-    {
-        icon: Sparkles,
-        title: "AI-Powered Generation",
-        description: "Transform text prompts into fully realized videos with our advanced AI algorithms.",
-    },
-    {
-        icon: FileEdit,
-        title: "Editable Narration",
-        description: "Customize the AI-generated narration to perfectly match your brand voice and style."
-    },
-    {
-        icon: Zap,
-        title: "Fast Rendering",
-        description: "Generate high-quality videos in minutes instead of hours or days."
-    },
-    {
-        icon: Video,
-        title: "Multiple Formats",
-        description: "Export videos in various formats optimized for social media, presentations, or websites."
-    },
-    {
-        icon: Globe,
-        title: "Multi-language Support",
-        description: "Create videos in multiple languages with accurate translations and native-sounding narration."
-    },
-    {
-        icon: Layout,
-        title: "Customizable Templates",
-        description: "Choose from dozens of pre-designed templates or create your own unique style."
-    }
-]
+  {
+    icon: Sparkles,
+    title: "AI-Powered Generation",
+    description:
+      "Transform text prompts into fully realized videos with our advanced AI algorithms.",
+  },
+  {
+    icon: FileEdit,
+    title: "Editable Narration",
+    description:
+      "Customize the AI-generated narration to perfectly match your brand voice and style.",
+  },
+  {
+    icon: Zap,
+    title: "Fast Rendering",
+    description:
+      "Generate high-quality videos in minutes instead of hours or days.",
+  },
+  {
+    icon: Video,
+    title: "Intuitive Line Captions",
+    description:
+      "Craft dynamic captions with multi-color styles and precise line control — perfect for boosting engagement across any platform.",
+  },
+  {
+    icon: Globe,
+    title: "Diverse Voice Profiles",
+    description:
+      "Choose from a rich variety of voice styles, accents, and tones to narrate videos from conversational to cinematic, every voice brings your story to life.",
+  },
+  {
+    icon: Layout,
+    title: "Customizable Templates",
+    description:
+      "Choose from dozens of pre-designed templates or create your own unique style.",
+  },
+];
