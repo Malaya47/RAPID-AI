ef86fce35a113546b9bf89104bc0c9feb4418a43
Malaya Tiwari <sidzilegend@gmail.com>
2025-08-04 14:22:26 +0530

    update: shifted video provider to layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,23 +1,23 @@
-import type React from "react"
-import { DashboardSidebar } from "@/components/dashboard/DashboardSidebar"
-import Link from "next/link"
-import { Video } from "lucide-react"
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import type React from "react";
+import { DashboardSidebar } from "@/components/dashboard/DashboardSidebar";
+import Link from "next/link";
+import { Video } from "lucide-react";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { VideoProvider } from "@/context/video-context";
 
 export default function DashboardLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <SidebarProvider>
-      <DashboardSidebar />
-      <div className="md:mx-auto p-5 bg-black w-full">
-        <main>
-          {children}
-        </main>
-      </div>
-    </SidebarProvider>
-  )
+    <VideoProvider>
+      <SidebarProvider>
+        <DashboardSidebar />
+        <div className="md:mx-auto p-5 bg-black w-full">
+          <main>{children}</main>
+        </div>
+      </SidebarProvider>
+    </VideoProvider>
+  );
 }
-
